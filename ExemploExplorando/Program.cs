b2b7b3e06eb6f7f1e65577769a7b373bcbe6ad12
Joao Victor Gregorio <joaogregoriodsilva@gmail.com>
2024-04-12 07:17:06 -0300

    Iniciado novo módulo de estudos sobre propriedades, métodos e construtores

---
+++ ExemploExplorando/Program.cs
@@ -0,0 +1,9 @@
+﻿using ExemploExplorando.Models;
+
+Console.Clear();
+
+Pessoa pessoaUm = new Pessoa();
+
+pessoaUm.Nome = "João Victor Gregório da Silva";
+pessoaUm.Idade = 24;
+pessoaUm.Apresentar();
