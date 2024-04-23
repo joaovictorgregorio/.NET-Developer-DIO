810c42f9a0a65b2db5f8039b938f42c156ce7c88
Joao Victor Gregorio <joaogregoriodsilva@gmail.com>
2024-04-23 16:59:01 -0300

    Desafio Construindo um Sistema de Hospedagem de um Hotel finalizado.

--- Manipulando Dados e Objetos com .NET/ExemploExplorando/obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
+++ Manipulando Dados e Objetos com .NET/ExemploExplorando/obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8daecf12ade7e10487d067374716d6642ec96e5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5780f94d8af01f1f22b9fd9e81afcba401e0ab88")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
