0db0756c7a7335354ced35a73e663fdbd47d21a1
Joao Victor Gregorio <joaogregoriodsilva@gmail.com>
2024-05-17 22:45:08 -0300

    Criando primeira API, desenvolvendo aplicação com entity framework dotnet versão 7

--- Programação de APIS com Bancos de Dados/ModuloAPI/obj/Debug/net7.0/ModuloAPI.AssemblyInfo.cs
+++ Programação de APIS com Bancos de Dados/ModuloAPI/obj/Debug/net7.0/ModuloAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ModuloAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f756ed90ce52136a76195fcc6a51f23295aa8361")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e25ba9e1c7fc1d16bf9a7d26aae0a6e6a3d91cd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ModuloAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ModuloAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
