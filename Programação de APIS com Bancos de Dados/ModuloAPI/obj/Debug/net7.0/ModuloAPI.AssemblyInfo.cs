9432aefd113e535927603dfdf2d04e139c9c965c
Joao Victor Gregorio <joaogregoriodsilva@gmail.com>
2024-05-23 15:08:49 -0300

    Desafio construindo um sistema de agendamento de tarefas com Entity Framework em desenvolvimento. Aplicação sendo desenvolvida com .NET 8. Controller Post ok; Controller Get por ID ok, GET listando todas tarefas ok e GET listar por titulo ok. Próximas etapas em construção.

--- Programação de APIS com Bancos de Dados/ModuloAPI/obj/Debug/net7.0/ModuloAPI.AssemblyInfo.cs
+++ Programação de APIS com Bancos de Dados/ModuloAPI/obj/Debug/net7.0/ModuloAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ModuloAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2262e8574b8910b5cabaff4104b7146d855f66b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bfbebe71d6753ccb136e19703dbbbe24b8825d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ModuloAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ModuloAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
